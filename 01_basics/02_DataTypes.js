b83047c10d51f4783ea120110b74bf6ff94083b2
Shivansu Kumar <91213279+shivansukumar@users.noreply.github.com>
2024-01-26 12:53:15 +0000

    dataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  // in nodejs it gives error, in broweser runs fine
+console.log("Shivansu")
+
+let name = "Shivansu" // string
+let age = 18; // Integer
+let isLoggedIn = true; // boolean
+
+// null --> It is a value not a datatype
+//undefied --> value is not assigned
+//symbol --> for uniqueness
+
+//object
+console.log(typeof("shivansu"));
+console.log(typeof(null));
