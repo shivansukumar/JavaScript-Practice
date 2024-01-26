1e521fc44f11d79f9784a15f2d39da2f0de3e24b
Shivansu Kumar <91213279+shivansukumar@users.noreply.github.com>
2024-01-26 12:44:40 +0000

    variabels

---
+++ 01_basics/01_variabels.js
@@ -0,0 +1,13 @@
+const accountId = 144554;
+let accountEmail = "shivisogo1212@mgail.com";
+var accountPassword = "12345";
+accountCity = "California"
+
+// accountId = 2; // not allowed
+
+accountEmail = "hc23@ad.com";
+accountPassword = "21213141";
+accountCity = "Gurgaon"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
