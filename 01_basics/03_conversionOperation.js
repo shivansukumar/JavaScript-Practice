d2ba7000bbd4b0772ce1f25343779d5d228cc2f3
Shivansu Kumar <91213279+shivansukumar@users.noreply.github.com>
2024-01-27 14:19:47 +0000

    DataType Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+let isLoggedIn = ""
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof(booleanIsLoggedIn))
+console.log(booleanIsLoggedIn);
+
+// 0 , "" --> Gives false boolean value
+// Apart from above two everything gives true value.
