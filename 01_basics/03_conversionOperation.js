0588fdfbfe2b06afcfa0c522cfb2e9e7b292fde0
Shivansu Kumar <91213279+shivansukumar@users.noreply.github.com>
2024-01-27 14:38:53 +0000

    Operations.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,8 +10,33 @@ let valueInNumber = Number(score);
 let isLoggedIn = ""
 
  let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof(booleanIsLoggedIn))
-console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn))
+// console.log(booleanIsLoggedIn);
 
 // 0 , "" --> Gives false boolean value
 // Apart from above two everything gives true value.
+
+// ************ Operations ***********
+let value = 3;
+let ngValue = -value;
+// console.log(ngValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**32);
+// console.log(2/2);
+
+let str1 = "hello"
+let str2 = "Shivansu"
+
+let str3 = str2 + str1
+// console.log(str3);
+
+// console.log(1-1 +" 1" );
+
+// console.log(+true)
+// console.log(-true)
+// console.log(+"")
+
+let num1, num2, num3
