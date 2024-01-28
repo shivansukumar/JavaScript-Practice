89bf285a1097c4e1d31116f1a5a6ca5a8a217ca0
Shivansu Kumar <91213279+shivansukumar@users.noreply.github.com>
2024-01-28 16:30:07 +0000

    intial

---
+++ 01_basics/06_Math_Num.js
@@ -0,0 +1,25 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const num = 23.845
+// console.log(num.toPrecision(2))
+
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//  ----------------X----------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.3));
+// console.log(Math.round(5.6));
+
+const val = Math.random()
+console.log(Math.random());
