245906f722cd32f5cdd096a833ece889351f6619
Shivansu Kumar <91213279+shivansukumar@users.noreply.github.com>
2024-02-01 09:22:06 +0000

    forOf

---
+++ 05_iterators/03_forOf.js
@@ -0,0 +1,34 @@
+//  for of
+
+const arr = [1,2,3,4,5];
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings =  "Hello World"
+// for(const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+
+// Maps
+const map = new Map();
+map.set("IN", "BHARAT");
+map.set("USA", "UNITED STATES OF AMERICA");
+map.set("USSR", "RUSSIA");
+map.set("IN", "BHARAT");
+// map.set("IN", "BHARAT");
+
+// console.log(map)
+
+// for(const [key,value] of map) {
+//     console.log(key, "--> ", value);
+// }
+
+const myObject = {
+    'game1' : 'NFS',
+    'game2' : "IGI",
+    'gmae3' : "Spiderman"
+}
+console.log(myObject);
