17794b94adc06bdf740ecf42adf007f4e0ecd122
Shivansu Kumar <91213279+shivansukumar@users.noreply.github.com>
2024-02-01 08:32:20 +0000

    loops

---
+++ 05_iterators/01_for.js
@@ -0,0 +1,16 @@
+// for
+
+// for(let i=0; i<10; i++) {
+//     console.log(`Outerloop value ${i}`)
+//     for(let j=0; j<=10; j++) {
+//         console.log(`Inner loop value ${j} `);
+//     }
+// }
+
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+for(let i=0; i<myArray.length; i++) {
+    console.log(myArray[i]);
+}
+
+
---
+++ 05_iterators/02_while.js
@@ -0,0 +1,12 @@
+// let i=0;
+// while(i < 5) {
+//     console.log(i);
+//     i++
+// }
+
+let score  = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score;
+}while(score <= 5);
