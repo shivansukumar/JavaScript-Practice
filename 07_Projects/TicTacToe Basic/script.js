4f5ffced2b0f7730955200c0450d671f3a66e890
shivansukumar <shivansugiri1998@gmail.com>
2024-02-18 23:44:54 +0530

    script-add

---
+++ 07_Projects/TicTacToe Basic/script.js
@@ -0,0 +1,43 @@
+console.log("Welcome to Tic Tac Toe");
+let music = new Audio("music.mp3");
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X"
+
+// Function to Change the turn
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X";
+}
+
+// Function to check for a win
+const checkWin = () => {
+    let boxtexts = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[0]].innerText))
+    })
+}
+
+// Game Logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        }
+    })
+})
